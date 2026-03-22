cd27b77e8caca8ace934bb6f8721805c3e51c0d0
Keepas3 <bfun679@gmail.com>
2026-03-21 23:49:24 -0400

    First commit making the skeleton of the website

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const BRAND_CONFIG = {
+  name: "Dragon Bot 2.0",
+  inviteLink: "https://discord.com/oauth2/authorize?client_id=1322658381974208522&permissions=2214709312&integration_type=0&scope=bot+applications.commands",
+  supportServer: "https://discord.gg/your-link",
+  githubOrg: "https://github.com/DragonBotProject"
+};
